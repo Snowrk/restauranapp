73a46b14f5d93e50209720f959a62a77274ad637
Snowrk <shinsan555555@gmail.com>
2024-05-03 00:16:58 +0530

    first commit from local

---
+++ src/pages/components/Header/index.js
@@ -0,0 +1,18 @@
+import { IoCartOutline } from "react-icons/io5";
+
+const Header = (props) => {
+  const { restaurant, cart } = props;
+  const restaurantName = restaurant.restaurant_name;
+  return (
+    <div className="header-container">
+      <h1>{restaurantName}</h1>
+      <p className="myorders">My Orders</p>
+      <div className="cart">
+        <IoCartOutline className="icon-size" />
+        <div className="no-of-items-in-cart">{cart}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
